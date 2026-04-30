711aad266cf5c2c5f1ca42a6bbe1d32206edd603
mapointi mapointi <marion.pointier-fourcart@learner.42.tech>
2026-04-30 11:54:33 +0200

    suite

--- HELPft_strnstr.c
+++ HELPft_strnstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   HELPft_strnstr.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mapointi <mapointi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 14:15:05 by mapointi          #+#    #+#             */
-/*   Updated: 2026/04/29 17:12:19 by mapointi         ###   ########.fr       */
+/*   Updated: 2026/04/30 11:53:37 by mapointi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,20 @@ char    *ft_strnstr(const char *big, const char* little, size_t len)
     size_t  j;
 
     i = 0;
-    j = 0; 
-    while(i < len && little[j] != '\0')
-    {
-        if(big == little)
-            return ((char *)little);
-        i++;   
-        j++;
+    while(i < len && big[i])
+    {   
+        if(little[j] == 0)    
+            return ((char *)big);
+        j = 0;
+        while(little[j] == big[i + j] && big[i + j] && (i + j) < len)
+        {      
+            j++;
+        }     
     } 
         if((char)little[0] == 0)
             return ((char *)big);
-        if(big != little)
-            return (NULL);
+        
+    return (NULL);
 }
 int main(void)
 {
